0daf522731a3e5eaba00fd73db6903233030a65b
benok <jovenvictorlogo@gmail.com>
2023-11-06 03:37:38 +0800

    feat: Install and configure the prettier and tailwindcss packages

---
+++ src/views/Welcome.tsx
@@ -0,0 +1,13 @@
+import Screen from '../components/Screen'
+
+const Welcome = () => {
+  return (
+    <div className="bg-sky-100">
+      <Screen>
+        <h1 className="text-4xl">Hello World!</h1>
+      </Screen>
+    </div>
+  )
+}
+
+export default Welcome
