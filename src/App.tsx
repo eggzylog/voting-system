3c64e4030e55afea2423a50b218a2f519bfb8b32
gestanestle <gesta.nestleo@gmail.com>
2023-11-07 03:49:46 +0800

    feat: setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Hackathon from "./components/Hackathon";
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  { path: "/", Component: Home },
+  { path: "*", Component: Root },
+]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+function Root() {
+  return (
+    <Routes>
+      <Route path="/hackathon/*" element={<Hackathon />} />
+    </Routes>
+  );
+}
