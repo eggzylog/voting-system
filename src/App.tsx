3cc06c6607f73add27783e2fee0138fbe065f0ab
gestanestle <gesta.nestleo@gmail.com>
2023-11-07 10:11:16 +0800

    feat: add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,46 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import AuthLayout from './layouts/AuthLayout'
-import GuestLayout from './layouts/GuestLayout'
 import Home from './views/Home'
 import About from './views/About'
 import Hackathon from './views/Hackathon'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <AuthLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
-      },
-      {
-        path: 'hackathon',
-        element: <Hackathon />
-      }
-    ]
-  },
-  {
-    path: '/signin',
-    element: <GuestLayout />
-  }
-])
+import { ClerkProvider, SignIn, SignUp } from '@clerk/clerk-react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 
-const App = () => {
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
+
+if (!clerkPubKey) {
+  throw new Error('Missing Publishable Key')
+}
+
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate()
+
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/hackathon" element={<Hackathon />} />
+        </Route>
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+      </Routes>
+    </ClerkProvider>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
   )
 }
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,7 @@ import ImageCard from '../components/ImageCard'
 const Home = () => {
   return (
     <>
-      <h1 className='uppercase text-center'>Hackathon Teams</h1>
+      <h1 className="text-center uppercase">Hackathon Teams</h1>
       <div className="grid grid-cols-12 gap-8 py-12">
         {Array.from({ length: 6 }).map((_, idx) => (
           <ImageCard key={idx} idx={idx} />
