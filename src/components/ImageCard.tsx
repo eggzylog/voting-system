e9233e8cb667a914b49e8ea671fba993dec65cba
benok <jovenvictorlogo@gmail.com>
2023-11-08 17:51:05 +0800

    fix: Protect the Vote button; Fix mobile responsiveness; Use alias in importing components

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import daedalus from '../assets/png/DAEDALUS-WHITE.png'
+import daedalus from '@/assets/png/DAEDALUS-WHITE.png'
+import { SignInButton, SignedIn, SignedOut } from '@clerk/clerk-react'
 
 const ImageCard = ({ idx }: { idx: number }) => {
   const [isHovered, setIsHovered] = useState(false)
@@ -9,13 +10,10 @@ const ImageCard = ({ idx }: { idx: number }) => {
       key={idx}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className={`card relative col-span-full h-96 w-full overflow-hidden shadow-xl md:col-span-6 lg:col-span-3 ${
+      className={`card relative col-span-full col-start-2 col-end-12 h-96 w-full overflow-hidden shadow-xl md:col-span-6 lg:col-span-4 xl:col-span-3 ${
         isHovered ? 'image-full' : ''
       }`}
     >
-      {/* <figure>
-        <img src="https://picsum.photos/400/200" alt={`Image ${idx}`} />
-      </figure> */}
       <div className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-[#04bfd87f] to-[#11113A] outline-[#39395B]">
         <img className="h-1/2 w-1/2" src={daedalus}></img>
         <h1 className="mt-3 font-bold text-white lg:text-3xl">D'Rocketeers</h1>
@@ -34,9 +32,23 @@ const ImageCard = ({ idx }: { idx: number }) => {
         </a>
         <h5>Members:</h5>
         <div className="card-actions justify-center">
-          <button className="btn w-full bg-white text-[#11113A] hover:bg-[#11113A] hover:text-white">
-            VOTE
-          </button>
+          {/* If the user is SignedOut, have the button show the SignIn modal when clicked */}
+          <SignedOut>
+            <SignInButton mode="modal" afterSignInUrl="/">
+              <button className="btn w-full bg-white text-[#11113A] hover:bg-[#11113A] hover:text-white">
+                VOTE
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <button
+              className="btn w-full bg-white text-[#11113A] hover:bg-[#11113A] hover:text-white"
+              // Add the Vote logic here 👇
+              onClick={() => console.log('vote iz clicked')}
+            >
+              VOTE
+            </button>
+          </SignedIn>
         </div>
       </div>
     </div>
