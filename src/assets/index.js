a48e738afb153365c4267ad6011e416d9a23ced9
Syntaxboybe <beboymegalbioeyana@gmail.com>
2023-11-11 13:58:10 +0800

    🧾docs: logo and hero changed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
-export { default as Daedalus } from './png/daedalus.png'
-export { default as Hero } from './png/hero-img.png'
+export { default as Daedalus } from './png/daedaluslogo-cover.png'
+export { default as Hero } from './png/daedalushero.png'
 export { default as Radial } from './png/radial.png'
 export { default as Man } from './png/man.png'
 export { default as Cube } from './png/cube.png'
