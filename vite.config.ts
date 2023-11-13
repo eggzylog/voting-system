6bad8c547a0f4e259a577ddb273a0a0b67ab2435
benok <jovenvictorlogo@gmail.com>
2023-11-13 21:07:27 +0800

    feat: Add vercel config (test)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-const HOST = 'http://178.128.20.38:8080'
+const HOST = process.env.VITE_SERVER_HOST
 
 // https://vitejs.dev/config/
 export default defineConfig({
